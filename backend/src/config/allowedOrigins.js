d9b750caec5d9fc3637c106d0e4df609baa5c2cb
Okenwa victor <OkenwaVictor003@gmail.com>
2025-03-22 22:20:15 +0100

    configure whitelist

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -1,7 +1,9 @@
-require('dotenv/config')
+require('dotenv/config');
+
 const whiteList = [
-    'http://localhost:8000', 
+    'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'https://waitlist-netmifi.vercel.app',
     process.env.FRONTEND_URL
 ];
 
