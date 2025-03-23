0791aa9977bfb2524929505d6412385f6900881a
Okenwa victor <OkenwaVictor003@gmail.com>
2025-03-23 15:41:18 +0100

    change cors option to fix cors issue

--- backend/src/config/corsOptions.js
+++ backend/src/config/corsOptions.js
@@ -20,7 +20,13 @@ const corsOptions = {
         callback(isAllowed ? null : new Error('CORS blocked'), isAllowed);
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Origin'],
+    headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    ],
     credentials: true,
     optionsSuccessStatus: 200
 };
