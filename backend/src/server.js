80fc5228d5645d5ad97ffae4e93f8c17c9f5cf4a
Okenwa victor <OkenwaVictor003@gmail.com>
2025-03-23 20:42:05 +0100

    exported app on server

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,33 +13,35 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const stageEnv = process.env.NODE_ENV;
 
+
 // app.use(require('./middlewares/credentials')) 
-// app.use(cors());
-// if (stageEnv === 'development') {
-//     app.use(
-//         helmet({
-//             contentSecurityPolicy: {
-//                 directives: {
-//                     defaultSrc: ["'self'", 'localhost:*'],
-//                     imgSrc: ["'self'", 'localhost:*', 'https://example.com'],
-//                     scriptSrc: ["'self'", 'https://trusted-scripts.com'],
-//                 },
-//             },
-//         })
-//     );
-// } else {
-//     app.use(helmet({
-//         contentSecurityPolicy: {
-//             directives: {
-//                 defaultSrc: ["'self'"],
-//                 scriptSrc: ["'self'", "https://waitlist-netmifi.vercel.app"],
-//                 connectSrc: ["'self'", "https://netmifi-waitlist.vercel.app"]
-//             }
-//         },
-//         crossOriginResourcePolicy: { policy: "cross-origin" }
-//     })); // Use default settings in production
-// }
-// app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+app.use(cors(corsOptions));
+if (stageEnv === 'development') {
+    app.use(
+        helmet({
+            contentSecurityPolicy: {
+                directives: {
+                    defaultSrc: ["'self'", 'localhost:*'],
+                    imgSrc: ["'self'", 'localhost:*', 'https://example.com'],
+                    scriptSrc: ["'self'", 'https://trusted-scripts.com'],
+                },
+            },
+        })
+    );
+} else {
+    app.use(helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'", "https://waitlist-netmifi.vercel.app"],
+                connectSrc: ["'self'", "https://netmifi-waitlist.vercel.app"]
+            }
+        },
+        crossOriginResourcePolicy: { policy: "cross-origin" }
+    })); // Use default settings in production
+}
 
 app.use(limiter);
 app.use(express.json())
@@ -52,5 +54,7 @@ app.use('/services', servicesRoutes);
 app.use('/waitlist', waitlistRoutes);
 
 connectToDb()
-    .then(() => app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`)))
-    .catch(console.dir);
+// .then(() => app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`)))
+// .catch(console.dir);
+
+module.exports = app
